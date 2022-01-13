4d8eefed7cf3f5393a95b8172109cfe9831e517f
IvanGerman <ivgvi@mail.ru>
2022-01-13 14:12:04 +0700

    3

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -176,8 +176,8 @@ function getHead(arr, n) {
  *    [ 1, 3, 4, 5 ], 2  => [ 4, 5 ]
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'b', 'c', 'd' ]
  */
-function getTail(arr, n) {
-  return arr.slice(arr.length - n, arr.length);
+function getTail(/* arr, n */) {
+  throw new Error('Not implemented');
 }
 
 
@@ -201,8 +201,8 @@ function getTail(arr, n) {
  *    +'20,21,22,23,24\n'
  *    +'30,31,32,33,34'
  */
-function toCsvText(arr) {
-  return arr.map((element) => element.join(',')).join('\n');
+function toCsvText(/* arr */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -216,8 +216,8 @@ function toCsvText(arr) {
  *   [ 0, 1, 2, 3, 4, 5 ] => [ 0, 1, 4, 9, 16, 25 ]
  *   [ 10, 100, -1 ]      => [ 100, 10000, 1 ]
  */
-function toArrayOfSquares(arr) {
-  return arr.map((element) => element ** 2);
+function toArrayOfSquares(/* arr */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -234,8 +234,8 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(arr) {
-  return arr.reduce((acc, current, element) => [...acc, acc.length > 0 ? current + acc[element - 1] : current], []);
+function getMovingSum(/* arr */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -249,8 +249,8 @@ function getMovingSum(arr) {
  * [ 'a', 'b', 'c' , null ]  => [ "b", null ]
  * [ "a" ] => []
  */
-function getSecondItems(arr) {
-  return arr.filter((element) => arr.indexOf(element) % 2 !== 0);
+function getSecondItems(/* arr */) {
+  throw new Error('Not implemented');
 }
 
 
