0c66fdadba4cb3555d3a7a121e1be66bc5943ad2
IvanGerman <ivgvi@mail.ru>
2022-01-14 19:21:55 +0700

    7

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -271,8 +271,11 @@ function getSecondItems(arr) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  return arr.reduce((acc, item, index) => {
+    const arr2 = Array(index + 1).fill(item);
+    return [...acc, ...arr2];
+  }, []);
 }
 
 
@@ -289,8 +292,9 @@ function propagateItemsByPositionIndex(/* arr */) {
  *   [ 1,2,3,4,5,6,7,8,9,10 ] => [ 10, 9, 8 ]
  *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
  */
-function get3TopItems(/* arr */) {
-  throw new Error('Not implemented');
+function get3TopItems(arr) {
+  const arr2 = arr.sort((a, b) => b - a);
+  return arr2.slice(0, 3);
 }
 
 
